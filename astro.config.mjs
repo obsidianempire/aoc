df8e00d75d0eb76bbb2ff919604d09227909ca32
Emperor of Obsidian <obsidianempireaoc@gmail.com>
2025-04-22 22:02:11 -0400

    add auto actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'static',
+    base: '/aoc/',
+    site: 'https://obsidianempire.github.io/aoc/',
+  });
